adf37adc8e7f7051b970da2ba4eb219a3b24137f
Taylor Barnes <taylor.a.barnes@gmail.com>
2024-07-15 16:37:05 -0400

    Update driver.py

--- tests/water/driver.py
+++ tests/water/driver.py
@@ -32,15 +32,44 @@ print("Node name: " + str(node_name))
 
 ##############
 print("AAA")
-#mdi.MDI_Send_Command("@INIT_MD", comm)
+mdi.MDI_Send_Command("@INIT_MD", comm)
 print("BBB")
 
 
 # Verify that the engine is still responsive
-#mdi.MDI_Send_Command("<NAME", comm)
-#final_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
-#assert initial_name == final_name
-#print("Engine name: " + str(final_name))
+mdi.MDI_Send_Command("<NAME", comm)
+final_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
+print("Final engine name: " + str(final_name))
+
+mdi.MDI_Send_Command("<NATOMS", comm)
+natoms = mdi.MDI_Recv(1, mdi.MDI_INT, comm)
+print("NATOMS: " + str(natoms))
+
+for iteration in range(20):
+    #print("Iteration: " + str(iteration))
+    mdi.MDI_Send_Command("@FORCES", comm)
+
+    #mdi.MDI_Send_Command("<@", comm)
+    #node_name = mdi.MDI_Recv(mdi.MDI_COMMAND_LENGTH, mdi.MDI_CHAR, comm)
+    #print("   NODE: " + str(node_name))
+
+    mdi.MDI_Send_Command("<KE", comm)
+    ke = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
+    print("   KE: " + str(ke))
+
+    #mdi.MDI_Send_Command("<PE", comm)
+    #pe = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
+    #print("   PE: " + str(pe))
+
+    mdi.MDI_Send_Command("<FORCES", comm)
+    forces = mdi.MDI_Recv(3 * natoms, mdi.MDI_DOUBLE, comm)
+    #print("   FORCES: " + str(forces))
+
+    forces = [ 0.0 for i in range(3*natoms) ]
+    mdi.MDI_Send_Command(">FORCES", comm)
+    mdi.MDI_Send(forces, 3 * natoms, mdi.MDI_DOUBLE, comm)
+    
+
 
 #mdi.MDI_Send_Command("@INIT_MD", comm)
 #for i in range(100):
